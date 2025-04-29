6f07c61a04e268a47d13fc53e97d4121e8d378d6
Rodrigo Arce C <rodrigoar87@gmail.com>
2025-04-29 12:32:23 -0600

    Add files via upload

--- soil.py
+++ soil.py
@@ -8,19 +8,24 @@ from time import sleep
 # Change the pin number as per your board (assuming A0 is GPIO36)
 ib = IdeaBoard()
 soil_sensor = ib.AnalogIn(board.IO33)
-#soil_sensor = DigitalInOut(board.IO33) 
-#soil_sensor.atten(ADC.ATTN_11DB)  # Set attenuation for max input voltage (3.3V)
 
-def read_soil_moisture():
+
+#def read_soil_moisture():
     # Read sensor value (0-4095 for 12-bit resolution)
-    moisture_value = soil_sensor.read()
+    #moisture_value = soil_sensor.read()
     
     # Convert to percentage (optional, depends on calibration)
-    moisture_percent = (moisture_value / 4095.0) * 100
-    return moisture_value, moisture_percent
+    #moisture_percent = (moisture_value / 4095.0) * 100
+#   return moisture_value, moisture_percent
 
 while True:
-    moisture_value, moisture_percent = read_soil_moisture()
+    print(soil_sensor.value)
+    time.sleep(0.5)
+    moisture_value = soil_sensor.value
+    moisture_percent = (moisture_value / 4095.0) * 100
+#soil_sensor = DigitalInOut(board.IO33) 
+#soil_sensor.atten(ADC.ATTN_11DB)  # Set attenuation for max input voltage (3.3V)
+ #   moisture_value, moisture_percent = read_soil_moisture()
     print("Soil Moisture (Raw Value):", moisture_value)
     print("Soil Moisture (%):", moisture_percent)
     
